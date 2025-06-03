1b9145e09afb022dd72035aea40a7a4754d13e97
Nischal <tamangnischal2018@gmail.com>
2025-06-03 20:23:02 +0545

    removed usestate

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import Cookies from 'js-cookie'
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 import { useGetProfile } from '@/query/auth/use-auth'
