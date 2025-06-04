f438e608fefd8c00fa834383837b574b43ad991d
Saroj Dahal <isarojdahal@gmail.com>
2025-06-04 19:24:44 +0545

    fix: build issue

--- src/features/oppourtunities/add/index.tsx
+++ src/features/oppourtunities/add/index.tsx
@@ -138,6 +138,7 @@ const AddOpportunity: FC = () => {
       <div className='mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0'>
         <div className='w-full'>
           <OpportunityForm
+            // @ts-expect-error opportunity form
             form={form}
             handleImageUpload={handleImageUpload}
             handleFormSubmit={handleFormSubmit}
