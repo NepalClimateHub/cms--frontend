3d1bf91f0219d655e2a00941a368c26e8cdc3bd6
Saroj Dahal <isarojdahal@gmail.com>
2025-07-01 00:29:55 +0545

    news edit form

--- src/features/news/edit/index.tsx
+++ src/features/news/edit/index.tsx
@@ -23,8 +23,8 @@ const NewsEdit = () => {
 
   const tagsOptions =
     tagsData?.data?.map((tag) => ({
-      value: tag.id,
-      label: tag.tag,
+    value: tag.id,
+    label: tag.tag,
     })) || []
 
   const newsMutation = useNewsAPI().updateNews
@@ -37,16 +37,16 @@ const NewsEdit = () => {
   useEffect(() => {
     if (newsData && !hasReset.current) {
       form.reset({
-        ...newsData,
-        publishedDate: newsData?.publishedDate
-          ? new Date(newsData?.publishedDate)
-          : new Date(),
+      ...newsData,
+      publishedDate: newsData?.publishedDate
+        ? new Date(newsData?.publishedDate)
+        : new Date(),
         // @ts-expect-error - tags mapping type mismatch
         tagIds: newsData?.tags?.map((tag: { id: string }) => tag?.id) || [],
-        bannerImageUrl: newsData?.bannerImageUrl
-          ? newsData?.bannerImageUrl
-          : null,
-      })
+      bannerImageUrl: newsData?.bannerImageUrl
+        ? newsData?.bannerImageUrl
+        : null,
+  })
       hasReset.current = true
       setIsFormReady(true)
     }
